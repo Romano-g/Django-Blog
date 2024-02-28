4461cc6f1579143983bdd5a75aa6f7ea1a0287c6
Caio Romano <caioromano13@gmail.com>
2024-02-28 15:21:05 -0300

    update post tags

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -66,12 +66,31 @@ def page(request, slug):
 
 
 def post(request, slug):
-    post = Post.objects.get_published().filter(slug=slug).first()  # type:ignore
+    post = (
+        Post.objects.get_published().filter(slug=slug).first()  # type:ignore
+    )
 
     return render(
         request,
         'blog/pages/post.html',
         {
             'post': post,
+        }
+    )
+
+
+def tag(request, slug):
+    posts = (
+        Post.objects.get_published().filter(tags__slug=slug)  # type:ignore
+    )
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
         }
     )
